cc4c230b75ab9d3d962a810f94b7b3e13aa3d09f
Samuel Elgozi <samu.elgozi@gmail.com>
2019-09-20 11:44:41 +0300

    chore: update dependencies and config

--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import errors from './errors';
  * https://firebase.google.com/docs/reference/rest/auth/
  */
 
+/**
+ * Returns a human readable error for a error code.
+ * @prop {string} error the error code from the rest API
+ * @returns {string} human readable error.
+ */
 function getHumanReadableError(error) {
 	// First check if the user is connected to the internet(this is not always correct).
-	if(!navigator.onLine) return 'You are not connected to the Internet.';
+	if (!navigator.onLine) return 'You are not connected to the Internet.';
 
 	// Try to get the correct message(sometimes in different places).
 	let errorMessage = error.error ? error.error.message : error.message;
@@ -23,7 +28,7 @@ function getHumanReadableError(error) {
  * The Fetch API throws only when there is a network connection issue, therefore
  * we need to manually check for the response status and throw an error ourselves.
  *
- * This function checkes that that the response object returns with the 'ok' boolean set to true,
+ * This function checks that that the response object returns with the 'ok' boolean set to true,
  * thats Fetch API's way of telling us that the response status is in the "successful" range.
  */
 function handleRequestErrors(response) {
@@ -33,11 +38,17 @@ function handleRequestErrors(response) {
 	return response;
 }
 
-/*
- * The public API
+/**
+ * Authentication class.
+ * Encapsulates logic for authentication and session
+ * management.
+ *
+ * @param {Object} config Firebase config object
+ * @param {string} config.apiKey The aPI key fot this firebase project
+ * @param {string} config.projectId   The project ID for this firebase project.
  */
-export default class {
-	constructor({apiKey, projectId}) {
+export default class Auth {
+	constructor({ apiKey, projectId }) {
 		this._endpoints = {
 			signUp: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=' + apiKey,
 			signIn: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyPassword?key=' + apiKey,
@@ -57,14 +68,17 @@ export default class {
 				method: 'POST',
 				body: JSON.stringify({ email, password, returnSecureToken: true }),
 				headers: { 'Content-Type': 'application/json' }
-			}).then(handleRequestErrors).then(response => response.json());
+			})
+				.then(handleRequestErrors)
+				.then(response => response.json());
+
+			console.log(authData);
 
 			// Calculate and add the expiration time of the token
 			authData.expirationDate = new Date(Number(authData.expiresIn) + requestTime * 1000);
 
 			// Save the session
 			this.saveSession(authData);
-
 		} catch (error) {
 			throw Error(getHumanReadableError(error));
 		}
@@ -76,22 +90,26 @@ export default class {
 	 */
 	async refreshIdToken() {
 		const session = this.session;
-		if(session === undefined) throw Error('Can\'t refresh the ID token because no session has been found');
+		if (session === undefined) throw Error("Can't refresh the ID token because no session has been found");
 
 		try {
 			let requestTime = Date.now(); // Used to calculate the expiration time of the token.
 			let refreshTokenData = await fetch(this._endpoints.token, {
 				method: 'POST',
-				body: JSON.stringify({ gran_token: 'refresh_token',refresh_token: session.refreshToken }),
+				body: JSON.stringify({
+					gran_token: 'refresh_token',
+					refresh_token: session.refreshToken
+				}),
 				headers: { 'Content-Type': 'application/json' }
-			}).then(handleRequestErrors).then(response => response.json());
+			})
+				.then(handleRequestErrors)
+				.then(response => response.json());
 
 			// Calculate and add the expiration time of the token
 			refreshTokenData.expirationDate = new Date(Number(refreshTokenData.expires_in) + requestTime * 1000);
 
 			// Update the session.
 			this.updateSession(refreshTokenData);
-
 		} catch (error) {
 			throw Error(getHumanReadableError(error));
 		}
@@ -102,7 +120,8 @@ export default class {
 	 */
 	saveSession(session) {
 		// Check that the session has an "expiration time".
-		if(Object.prototype.toString.call(session.expirationDate) !== '[object Date]') throw Error('The session should have a valid expiration date');
+		if (Object.prototype.toString.call(session.expirationDate) !== '[object Date]')
+			throw Error('The session should have a valid expiration date');
 
 		// Save the session on memory(in this instance of the class)
 		this._session = session;
@@ -116,10 +135,14 @@ export default class {
 	 */
 	updateSession(refreshTokenResponse) {
 		// Extract the data
-		let {expires_in: expiresIn, refresh_token: refreshToken, id_token: idToken } = refreshTokenResponse;
+		let { expires_in: expiresIn, refresh_token: refreshToken, id_token: idToken } = refreshTokenResponse;
 
 		// Create a new object with the updated data.
-		const updatedSession = Object.assign(this.session, {expiresIn, refreshToken, idToken});
+		const updatedSession = Object.assign(this.session, {
+			expiresIn,
+			refreshToken,
+			idToken
+		});
 
 		// Save the new session object.
 		this.saveSession(updatedSession);
@@ -144,7 +167,7 @@ export default class {
 		const session = this._session || JSON.parse(localStorage.getItem(this._sessionKey));
 
 		// If no session found, return undefined.
-		if(!session) return undefined;
+		if (!session) return undefined;
 
 		// If a session was found, return it.
 		return session;
@@ -158,7 +181,7 @@ export default class {
 	get user() {
 		const session = this.session;
 		// if there is no session then return undefined.
-		if(this.session === undefined) return undefined;
+		if (this.session === undefined) return undefined;
 
 		// If there is a session then create a new object with only the relevant info.
 		let { displayName, email, localId, registered } = session;
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,5 +1,9 @@
 import AuthAPI from '../src/index.js';
-import config from '../config';
+import config from '../config.js';
+import fetch from 'node-fetch';
+
+window.fetch = fetch;
+
 /*
  * In order to test this package you need to create a config.js file in the root folder
  * that exports a firebase web configuration object with the apiKey and projectId.
@@ -11,9 +15,22 @@ import config from '../config';
  * Also you need to have a username with the email: test@test.com
  * And a password: 123456
  *
- * This will be required untill i have time to mock the API.
+ * This will be required until i have time to mock the API.
  */
 const auth = new AuthAPI(config);
+
+test("Throws error is used doesn't exist", () => {
+	expect.assertions(4);
+
+	return auth.signIn('test@test.com', '123456').then(() => {
+		const session = JSON.parse(localStorage.getItem(auth._sessionKey));
+
+		expect(session).toBeDefined();
+		expect(session).toHaveProperty('email', 'test@test.com');
+		expect(session).toHaveProperty('kind', 'identitytoolkit#VerifyPasswordResponse');
+		expect(session).toHaveProperty('registered', true);
+	});
+});
 
 test('Saves session into the LocalStorage after logging-in', () => {
 	expect.assertions(4);
