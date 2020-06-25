43b0232cc872f8c02dcb6948dda2546af535169b
Dair Aidarkhanov <36963426+aidarkhanov@users.noreply.github.com>
2020-06-25 12:43:24 +0600

    docs: fix typos in jsdocs and update dependencies (#38)
    * chore: updated dependencies
    
    * chore: documentation and code clean up
    
    * fix: adjusted tests
    
    * fix: reverted code changes
    
    * fix: adjusted tests
    
    * chore: updated dependencies
    
    * chore: reduced code size
    
    * chore: return parentheses around await blocks

--- src/main.js
+++ src/main.js
@@ -4,50 +4,50 @@
  */
 
 /**
- * Settings object for an IDP(Identity Provider).
+ * Settings object for an IdP (Identity Provider).
  * @typedef {Object} ProviderOptions
- * @property {string} options.name The name of the provider in lowercase.
- * @property {string} [options.scope] The scopes for the IDP, this is optional and defaults to "openid email".
+ * @property {string} options.name Name of the provider in lowercase.
+ * @property {string} [options.scope] Scopes for the IdP, this is optional and defaults to "OpenID email".
  */
 
 /**
  * Object response from a "fetchProvidersForEmail" request.
  * @typedef {Object} ProvidersForEmailResponse
- * @property {Array.<string>} allProviders All providers the user has once used to do federated login
+ * @property {Array.<string>} allProviders All providers the user has once used to do federated sign-in.
  * @property {boolean} registered All sign-in methods this user has used.
- * @property {string} sessionId Session ID which should be passed in the following verifyAssertion request
+ * @property {string} sessionId Session ID which should be passed in the following verifyAssertion request.
  * @property {Array.<string>} signinMethods All sign-in methods this user has used.
  */
 
 /**
- * Setting object for the "startOauthFlow" method.
+ * Settings object for the "startOauthFlow" method.
  * @typedef {Object} oauthFlowOptions
  * @property {string} provider Name of the provider to use.
- * @property {string} [context] A string that will be returned after the Oauth flow is finished, should be used to retain context.
- * @property {boolean} [linkAccount = false] Whether to link this oauth account with the current account. defaults to false.
+ * @property {string} [context] A string that will be returned after the OAuth flow is finished should be used to retain context.
+ * @property {boolean} [linkAccount = false] Check whether to link this OAuth account with the current account. Defaults to false.
  */
 
-// Generate a local storage adapter.
-// Its a bit verbose, but takes less characters than writing it manually.
+// Generates a localStorage adapter.
+// It's a bit verbose, but takes less characters than writing it manually.
 const storageApi = {};
 ['set', 'get', 'remove'].forEach(m => (storageApi[m] = async (k, v) => localStorage[m + 'Item'](k, v)));
 
 /**
  * Encapsulates authentication flow logic.
  * @param {Object} options Options object.
- * @param {string} options.apiKey The firebase API key
+ * @param {string} options.apiKey The Firebase API key.
  * @param {string} options.redirectUri The redirect URL used by OAuth providers.
  * @param {Array.<ProviderOptions|string>} options.providers Array of arguments that will be passed to the addProvider method.
  */
 export default class Auth {
-	constructor({ name = 'default', apiKey, redirectUri, storage = storageApi } = {}) {
-		if (typeof apiKey !== 'string') throw Error('The argument "apiKey" is required');
+	constructor({ apiKey, redirectUri, name = 'default', storage = storageApi } = {}) {
+		if (!apiKey) throw Error('The argument "apiKey" is required');
 
 		Object.assign(this, {
-			name,
 			apiKey,
-			storage,
 			redirectUri,
+			name,
+			storage,
 			listeners: []
 		});
 
@@ -62,8 +62,8 @@ export default class Auth {
 					});
 		});
 
-		// Because this library is used in react native, outside the browser as well,
-		// we need to first check if this environment supports `addEventListener` on the window.
+		// Because this library is also used in React Native, outside the browser as well,
+		// we need to check if this environment supports `addEventListener` on the window.
 		'addEventListener' in window &&
 			window.addEventListener('storage', e => {
 				// This code will run if localStorage for this user
@@ -75,8 +75,8 @@ export default class Auth {
 
 	/**
 	 * Emits an event and triggers all of the listeners.
-	 * @param {string} name The name of the event to trigger.
-	 * @param {any} data The data you want to pass to the event listeners.
+	 * @param {string} name Name of the event to trigger.
+	 * @param {any} data Data you want to pass to the event listeners.
 	 * @private
 	 */
 	emit() {
@@ -84,9 +84,9 @@ export default class Auth {
 	}
 
 	/**
-	 * Set up a function that will be called whenever the user state is changed.
-	 * @param {function} cb The function to call when the event is triggered.
-	 * @returns {function} function that will unsubscribe your callback from being called.
+	 * Sets up a function that will be called whenever the user state is changed.
+	 * @param {function} callback Function to call when the event is triggered.
+	 * @returns {function} The function that unsubscribes your callback after being called.
 	 */
 	listen(cb) {
 		this.listeners.push(cb);
@@ -104,8 +104,8 @@ export default class Auth {
 	}
 
 	/**
-	 * Make post request to a specific endpoint, and return the response.
-	 * @param {string} endpoint The name of the endpoint.
+	 * Makes a post request to a specific endpoint and returns the response.
+	 * @param {string} endpoint Name of the endpoint.
 	 * @param {any} request Body to pass to the request.
 	 * @private
 	 */
@@ -137,19 +137,19 @@ export default class Auth {
 	}
 
 	/**
-	 * Makes sure the user is logged in and has up-to-date credentials.
-	 * @throws Will throw if the user is not logged in.
+	 * Makes sure the user is signed-in and has up-to-date credentials.
+	 * @throws Will throw if the user is not signed-in.
 	 * @private
 	 */
 	async enforceAuth() {
-		if (!this.user) throw Error('The user must be logged-in to use this method.');
+		if (!this.user) throw Error('The user must be signed-in to use this method.');
 		return this.refreshIdToken(); // Won't do anything if the token is valid.
 	}
 
 	/**
-	 * Updates the user data in the localStorage.
-	 * @param {Object} userData the new user data.
-	 * @param {boolean} [updateStorage = true] Whether to update local storage or not.
+	 * Updates the user data in localStorage.
+	 * @param {Object} userData New user data.
+	 * @param {boolean} [updateStorage = true] Check whether to update localStorage or not.
 	 * @private
 	 */
 	async setState(userData, persist = true, emit = true) {
@@ -159,7 +159,7 @@ export default class Auth {
 	}
 
 	/**
-	 * Sign out the currently signed in user.
+	 * Sign out the currently signed-in user.
 	 * Removes all data stored in the storage that's associated with the user.
 	 */
 	signOut() {
@@ -167,22 +167,20 @@ export default class Auth {
 	}
 
 	/**
-	 * Refreshes the idToken by using the locally stored refresh token
-	 * only if the idToken has expired.
+	 * Refreshes the idToken by using the locally stored refresh token only if the idToken has expired.
 	 * @private
 	 */
 	async refreshIdToken() {
 		// If the idToken didn't expire, return.
 		if (Date.now() < this.user.tokenManager.expiresAt) return;
 
-		// If a request for a new token was already made, then wait for it and then return.
-		if (this._ref) {
-			return void (await this._ref);
-		}
+		// If the request for a new token was already made, then wait for it and return.
+		if (this._ref) return void (await this._ref);
 
+		// If the idToken is expired or the request for a new token was made, then refresh.
 		try {
-			// Save the promise so that if this function is called
-			// anywhere else we don't make more than one request.
+			// Save the promise when this function is called,
+			// else we don't make more than one request.
 			this._ref = this.api('token', {
 				grant_type: 'refresh_token',
 				refresh_token: this.user.tokenManager.refreshToken
@@ -201,9 +199,9 @@ export default class Auth {
 	}
 
 	/**
-	 * Uses native fetch, but adds authorization headers otherwise the API is exactly the same as native fetch.
-	 * @param {Request|Object|string} resource the resource to send the request to, or an options object.
-	 * @param {Object} init an options object.
+	 * Uses native fetch, but adds authorization headers, otherwise, the API is exactly the same as native fetch.
+	 * @param {Request|Object|string} resource A request to send. It can be a resource or an options object.
+	 * @param {Object} init An options object.
 	 */
 	async authorizedRequest(resource, init) {
 		const request = resource instanceof Request ? resource : new Request(resource, init);
@@ -217,7 +215,7 @@ export default class Auth {
 	}
 
 	/**
-	 * Signs in or signs up a user by exchanging a custom Auth token.
+	 * Signs in or signs up a user by exchanging a custom authentication token.
 	 * @param {string} token The custom token.
 	 */
 	async signInWithCustomToken(token) {
@@ -232,20 +230,22 @@ export default class Auth {
 	}
 
 	/**
-	 * Start auth flow of a federated Id provider.
-	 * Will redirect the page to the federated login page.
-	 * @param {oauthFlowOptions|string} options An options object, or a string with the name of the provider.
+	 * Starts the auth flow of a federated ID provider.
+	 * Also, it will redirect the page to the federated sign-in page.
+	 * @param {oauthFlowOptions|string} options An options object or a string with the name of the provider.
 	 */
 	async signInWithProvider(options) {
 		if (!this.redirectUri)
-			throw Error('In order to use an Identity provider you should initiate the "Auth" instance with a "redirectUri".');
+			throw Error(
+				'In order to use an Identity provider, you should initiate the "Auth" instance with a "redirectUri".'
+			);
 
 		// The options can be a string, or an object, so here we make sure we extract the right data in each case.
 		const { provider, oauthScope, context, linkAccount } =
 			typeof options === 'string' ? { provider: options } : options;
 
 		// Make sure the user is logged in when an "account link" was requested.
-		if (linkAccount) await this.enforceAuth();
+		linkAccount && (await this.enforceAuth());
 
 		// Get the url and other data necessary for the authentication.
 		const { authUri, sessionId } = await this.api('createAuthUri', {
@@ -269,8 +269,8 @@ export default class Auth {
 
 	/**
 	 * Signs in or signs up a user using credentials from an Identity Provider (IdP) after a redirect.
-	 * Will fail silently if the URL doesn't have a "code" search param.
-	 * @param {string} [requestUri] The request URI with the authorization code, state etc. from the IdP.
+	 * It will fail silently if the URL doesn't have a "code" search param.
+	 * @param {string} [requestUri] The request URI with the authorization code, state, etc. from the IdP.
 	 * @private
 	 */
 	async finishProviderSignIn(requestUri = location.href) {
@@ -278,21 +278,23 @@ export default class Auth {
 		const sessionId = await this.storage.get(this.sKey('SessionId'));
 		// Get the indication if this was a "link account" request.
 		const linkAccount = await this.storage.get(this.sKey('LinkAccount'));
-		// Check for the edge case in which the user signed out before completing the linkAccount
-		// Request.
+
+		// Check for the edge case in which the user signed-out
+		// before completing the linkAccount request.
 		if (linkAccount && !this.user) throw Error('Request to "Link account" was made, but user is no longer signed-in');
+
 		await this.storage.remove(this.sKey('LinkAccount'));
 
 		// Try to exchange the Auth Code for an idToken and refreshToken.
 		const { idToken, refreshToken, expiresAt, context } = await this.api('signInWithIdp', {
-			// If this is a "link account" flow, then attach the idToken of the currently logged in account.
+			// If this is a "link account" flow, then attach the idToken of the currently signed-in account.
 			idToken: linkAccount ? this.user.tokenManager.idToken : undefined,
 			requestUri,
 			sessionId,
 			returnSecureToken: true
 		});
 
-		// Now get the user profile.
+		// Now, get the user profile.
 		await this.fetchProfile({ idToken, refreshToken, expiresAt });
 
 		// Remove sensitive data from the URLSearch params in the location bar.
@@ -302,21 +304,23 @@ export default class Auth {
 	}
 
 	/**
-	 * Handles all sign in flows that complete via redirects.
+	 * Handles all sign-in flows that complete via redirects.
 	 * Fails silently if no redirect was detected.
 	 */
 	async handleSignInRedirect() {
-		// Oauth Federated Identity Provider flow.
+		// OAuth Federated Identity Provider flow.
 		if (location.href.match(/[&?]code=/)) return this.finishProviderSignIn();
 
-		// Email Sign-in flow.
+		// Email sign-in flow.
 		if (location.href.match(/[&?]oobCode=/)) {
 			const oobCode = location.href.match(/[?&]oobCode=([^&]+)/)[1];
 			const email = location.href.match(/[?&]email=([^&]+)/)[1];
 			const expiresAt = Date.now() + 3600 * 1000;
 			const { idToken, refreshToken } = await this.api('signInWithEmailLink', { oobCode, email });
-			// Now get the user profile.
+
+			// Now, get the user profile.
 			await this.fetchProfile({ idToken, refreshToken, expiresAt });
+
 			// Remove sensitive data from the URLSearch params in the location bar.
 			history.replaceState(null, null, location.origin + location.pathname);
 		}
@@ -325,11 +329,11 @@ export default class Auth {
 	/**
 	 * Signs up with email and password or anonymously when no arguments are passed.
 	 * Automatically signs the user in on completion.
-	 * @param {string} [email] The email for the user to create.
-	 * @param {string} [password] The password for the user to create.
+	 * @param {string} [email] An email for the user to pass.
+	 * @param {string} [password] A password for the user to pass.
 	 */
 	async signUp(email, password) {
-		// Sign up and then retrieve the user profile and persists the session.
+		// Sign up and then retrieve the user profile and persist it in the session.
 		return await this.fetchProfile(
 			await this.api('signUp', {
 				email,
@@ -345,7 +349,7 @@ export default class Auth {
 	 * @param {string} password
 	 */
 	async signIn(email, password) {
-		// Sign up and then retrieve the user profile and persists the session.
+		// Sign in and then retrieve the user profile and persist it in the session.
 		return await this.fetchProfile(
 			await this.api('signInWithPassword', {
 				email,
@@ -357,8 +361,8 @@ export default class Auth {
 
 	/**
 	 * Sends an out-of-band confirmation code for an account.
-	 * Can be used to reset a password, to verify an email address and send a Sign-in email link.
-	 * The `email` argument is not needed only when verifying an email(In that case it will be completely ignored, even if specified), otherwise it is required.
+	 * It can be used to reset a password, to verify an email address and send a sign-in email link.
+	 * The email argument is not needed if verifying an email (the argument is ignored). Otherwise, it is required.
 	 * @param {'PASSWORD_RESET'|'VERIFY_EMAIL'|'EMAIL_SIGNIN'} requestType The type of out-of-band (OOB) code to send.
 	 * @param {string} [email] When the `requestType` is `PASSWORD_RESET` or `EMAIL_SIGNIN` you need to provide an email address.
 	 * @returns {Promise}
@@ -379,8 +383,8 @@ export default class Auth {
 	}
 
 	/**
-	 * Sets a new password by using a reset code.
-	 * Can also be used to very oobCode by not passing a password.
+	 * Resets the password by using a reset code.
+	 * It can also be used to verify oobCode by not passing a password.
 	 * @param {string} code
 	 * @returns {string} The email of the account to which the code was issued.
 	 */
@@ -401,13 +405,13 @@ export default class Auth {
 
 	/**
 	 * Gets the user data from the server, and updates the local caches.
-	 * @param {Object} [tokenManager] Only when not logged in.
-	 * @throws Will throw if the user is not signed in.
+	 * @param {Object} [tokenManager] Only when not signed-in.
+	 * @throws Will throw if the user is not signed-in.
 	 */
 	async fetchProfile(tokenManager = this.user && this.user.tokenManager) {
 		if (!tokenManager) await this.enforceAuth();
 
-		const userData = (await this.api('lookup', { idToken: tokenManager.idToken })).users[0];
+		const [userData] = (await this.api('lookup', { idToken: tokenManager.idToken })).users;
 
 		delete userData.kind;
 		userData.tokenManager = tokenManager;
@@ -416,9 +420,9 @@ export default class Auth {
 	}
 
 	/**
-	 * Update user's profile.
-	 * @param {Object} newData An object with the new data to overwrite.
-	 * @throws Will throw if the user is not signed in.
+	 * Updates the user's profile.
+	 * @param {Object} newData An object with the new data.
+	 * @throws Will throw if the user is not signed-in.
 	 */
 	async updateProfile(newData) {
 		await this.enforceAuth();
@@ -446,8 +450,8 @@ export default class Auth {
 	}
 
 	/**
-	 * Deletes the currently logged in account and logs out.
-	 * @throws Will throw if the user is not signed in.
+	 * Deletes the currently signed-in account then sign out.
+	 * @throws Will throw if the user is not signed-in.
 	 */
 	async deleteAccount() {
 		await this.enforceAuth();
